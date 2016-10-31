b65ba420c2644eb94571cc86d835391963b4ceac
denys5dev <denys5dev@gmail.com>
2016-10-31 21:26:28 +0200

    optimized

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,18 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
-import { MessageComponent } from './messages/message.component';
-import { MessageListComponent } from './messages/message-list.component';
-import { MessageInputComponent } from './messages/message-input.component';
-import { MessagesComponent } from './messages/messages.component';
+
+import { MessageModule } from './messages/message.module';
 import { AuthenticationComponent } from './auth/authentication.component';
-import { SigninComponent } from './auth/signin.component';
-import { SignupComponent } from './auth/signup.component';
-import { LogoutComponent } from './auth/logout.component';
+
 import { routing } from './app.routing';
 import { AuthService } from './auth/auth.service';
 import { ErrorComponent } from './errors/error.component';
@@ -22,22 +17,14 @@ import { ErrorService } from './errors/error.service';
     declarations: [
         AppComponent,
         HeaderComponent,
-        MessageComponent,
-        MessageListComponent,
-        MessageInputComponent,
-        MessagesComponent,
         AuthenticationComponent,
-        SigninComponent,
-        SignupComponent,
-        LogoutComponent,
         ErrorComponent
     ],
     imports: [
         BrowserModule, 
-        FormsModule, 
         routing, 
-        ReactiveFormsModule,
-        HttpModule
+        HttpModule,
+        MessageModule
     ],
     providers: [AuthService, ErrorService],
     bootstrap: [AppComponent]
---
+++ assets/app/auth/auth.routing.ts
@@ -0,0 +1,14 @@
+import { Routes, RouterModule } from '@angular/router';
+
+import { SignupComponent } from './signup.component';
+import { SigninComponent } from './signin.component';
+import { LogoutComponent } from './logout.component';
+
+const AUTH_ROUTES: Routes = [
+    { path: "signup", component: SignupComponent },
+    { path: "signin", component: SigninComponent },
+    { path: "logout", component: LogoutComponent },
+    { path: "", redirectTo: 'signup', pathMatch: 'full' }
+];
+
+export const authRouting = RouterModule.forChild(AUTH_ROUTES);
